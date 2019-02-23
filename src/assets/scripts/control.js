e0f4e01750d476c8f6291c0bb0fb8cb563ed622b
Ari <aristath@gmail.com>
2019-02-24 00:49:37 +0200

    add kirki.input.radio

--- src/assets/scripts/control.js
+++ src/assets/scripts/control.js
@@ -1,2 +1,23 @@
+var kirki = kirki || {};
+kirki.input = kirki.input || {};
+kirki.input.radio = {
+    /**
+     * Init the control.
+     *
+     * @since 3.0.17
+     * @param {Object} control - The control object.
+     * @param {Object} control.id - The setting.
+     * @returns {null}
+     */
+    init: function( control ) {
+        var input = jQuery( 'input[data-id="' + control.id + '"]' );
+
+        // Save the value
+        input.on( 'change keyup paste click', function() {
+            kirki.setting.set( control.id, jQuery( this ).val() );
+        } );
+    }
+}
+
 wp.customize.controlConstructor['kirki-radio-buttonset'] = wp.customize.kirkiDynamicControl.extend( {} );
 wp.customize.controlConstructor['kirki-radio-image'] = wp.customize.kirkiDynamicControl.extend( {} );
