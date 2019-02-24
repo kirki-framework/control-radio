9986a9907e2f019f85898230a72f597d7ee94ab3
Ari <aristath@gmail.com>
2019-02-24 12:52:09 +0200

    add view

--- src/functions.php
+++ src/functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Class aliases for backwards-compatibility.
+ * Add extra functions and hooks.
  *
  * @package    Kirki
  * @copyright  Copyright (c) 2019, Ari Stathopoulos (@aristath)
@@ -14,5 +14,15 @@ add_action(
         class_alias( 'Kirki\Control\Radio', 'Kirki_Control_Radio' );
         class_alias( 'Kirki\Control\Radio_Buttonset', 'Kirki_Control_Radio_Buttonset' );
         class_alias( 'Kirki\Control\Radio_Image', 'Kirki_Control_Radio_Image' );
+    }
+);
+
+add_action(
+    'customize_controls_print_footer_scripts',
+    function() {
+        $path = apply_filters( 'kirki_control_view_radio', __DIR__ . '/view.php' );
+        echo '<script type="text/html" id="tmpl-kirki-input-radio">';
+        include $path;
+        echo '</script>';
     }
 );
